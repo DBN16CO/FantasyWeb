f8bc0615469581593c0d929d0b4942b638dcbac8
Drew Kowalski <drewkowalski93@gmail.com>
2018-08-18 22:15:31 -0400

    add secret settings

--- FantasyWeb/settings.py
+++ FantasyWeb/settings.py
@@ -12,11 +12,17 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
-from .settings_secret import *
+
+try:
+    from .settings_secret import *
+except Exception:
+    pass
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
